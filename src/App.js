f28b4f4765662bf05e7967d5cdc125cdfc57dfc3
Alejandro Capote <alexkpot@MC-117-MacBook-Pro.local>
2023-01-21 18:57:15 -0500

    Dark mode implemented V1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Header from './components/Header'
 import Links from './components/Links'
+import Footer from './components/Footer'
 import itemsData from './components/items/itemsData'
+import useLocalStorage from 'use-local-storage'
 
 function App() {
   const itemsDataList = itemsData
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  )
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
 
   return (
-    <div>
+    <div className='app' data-theme={theme}>
+      <button onClick={switchTheme}>switch theme</button>
       <Header />
       <Links items={itemsDataList} />
+      <Footer />
     </div>
   )
 }
